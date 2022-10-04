84390547f957fe0b722dac323a6d7d88e755d748
Emilio Righi <righiemilio@gmail.com>
2022-10-04 10:51:26 +0200

    fix vite config

--- biogenome-client/vite.config.js
+++ biogenome-client/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import cesium from 'vite-plugin-cesium';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
@@ -6,10 +6,9 @@ import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfil
 import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
-const basePath = import.meta.env.VITE_BASE_PATH
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: basePath,
+  base: loadEnv(mode, process.cwd(), '').VITE_BASE_PATH,
   resolve: {
     alias: {
       stream: 'stream-browserify',
