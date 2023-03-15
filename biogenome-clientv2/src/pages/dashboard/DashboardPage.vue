f23d5e18b27de9049b1280a361ee83217795f7b9
“Emilio <righiemilio@gmail.com>
2023-03-15 11:47:31 +0100

    dashboard page done

--- biogenome-clientv2/src/pages/dashboard/DashboardPage.vue
+++ biogenome-clientv2/src/pages/dashboard/DashboardPage.vue
@@ -4,67 +4,25 @@
   
         <DashboardCharts />
         <div class="row row-equal">
-            <div class="flex lg6 md6 sm12 xs12">
+            <div style="min-height: 400px;" class="flex lg6 md6 sm12 xs12">
                 <Suspense>
                     <DashBoardMap/>
                 </Suspense>
             </div>
-            <div v-if="showTree" class="flex lg6 md6 sm12 xs12">
-                <va-card>
-                    <va-card-title>
-                        phylogenetic tree
-                    </va-card-title>
-                    <va-card-content style="overflow: scroll;">
-                        <TreeOfLife :data="treeData"/>
-                    </va-card-content>
-                </va-card>
+            <div class="flex lg6 md6 sm12 xs12">
+                <Suspense>
+                    <DashboardTree/>
+                </Suspense>
             </div>
         </div>
-
-<!--   
-      <div class="row row-equal">
-        <div class="flex xs12 lg6">
-          <dashboard-tabs @submit="addAddressToMap" />
-        </div>
-  
-        <div class="flex xs12 lg6">
-          <DashboardMap ref="dashboardMap" />
-        </div>
-      </div> -->
     </div>
-        <!-- 
-        all stats
-        organisms map
-        top ranks
-        insdc status
-        goat status
-
-     -->
 </template>
 
 <script setup lang="ts">
 import DashboardInfoBlock from './DashboardInfoBlock.vue';
 import DashboardCharts from './DashboardCharts.vue';
-import OrganismService from '../../services/clients/OrganismService';
 import DashBoardMap from './DashBoardMap.vue';
-import TreeOfLife from '../../components/TreeOfLife.vue';
-import { onMounted, ref } from 'vue';
-import TaxonService from '../../services/clients/TaxonService';
-
-
-const showTree = ref(false)
-const treeData = ref({})
-onMounted(async () => {
-    getTreeData('2759')
-  })
-
-  async function getTreeData(taxid: string) {
-    showTree.value = false
-    const { data } = await TaxonService.getTree(taxid, { limit: 166 })
-    treeData.value = {...data}
-    showTree.value = true
-    return data
-  }
+import DashboardTree from './DashboardTree.vue';
 
 </script>
 <style lang="scss">
@@ -82,5 +40,8 @@ onMounted(async () => {
         justify-content: space-between;
       }
     }
+  }
+  .chart{
+    height: 400px;
   }
 </style>
---
+++ biogenome-clientv2/src/pages/dashboard/DashboardTree.vue
@@ -0,0 +1,17 @@
+<template>
+    <va-card >
+        <va-card-title>
+            phylogenetic tree
+        </va-card-title>
+        <va-card-content class="chart" style="overflow: scroll;">
+            <IndentedTree :data="data"/>
+        </va-card-content>
+    </va-card>
+</template>
+<script setup lang="ts">
+import TaxonService from '../../services/clients/TaxonService';
+import IndentedTree from '../organisms/IndentedTree.vue';
+
+const {data} = await TaxonService.getTree('131567')
+
+</script>
