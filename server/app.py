f717ab04a58959b18e0dda2f5061797a432a6bb1
Emilio Righi <righiemilio@gmail.com>
2024-03-11 11:26:02 +0100

    add refresh token wrapper

--- server/app.py
+++ server/app.py
@@ -2,23 +2,26 @@ from flask import Flask
 from flask_cors import CORS
 from config import BaseConfig
 from rest import initialize_api
-from flask_jwt_extended import JWTManager
-from db.models import BioGenomeUser,GenomeAnnotation, CronJob,Assembly,Roles,SampleCoordinates,BioSample,LocalSample,Chromosome, Organism,Experiment,TaxonNode
+from flask_jwt_extended import JWTManager,get_jwt, create_access_token, get_jwt_identity, set_access_cookies
+from db.models import BioGenomeUser, CronJob,Roles
 from tendo.singleton import SingleInstance
 from flask_mongoengine import MongoEngine
+from datetime import datetime
+from datetime import timedelta
+from datetime import timezone
 
 import os
 
 
 app = Flask(__name__)
 
+
 app.config.from_object(BaseConfig)
 app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
 
-# If true this will only allow the cookies that contain your JWTs to be sent
-# over https. In production, this should always be set to True
+
 app.config["JWT_COOKIE_SAMESITE"] = "None"
-# app.config["JWT_COOKIE_SECURE"] = True
+app.config["JWT_COOKIE_SECURE"] = True
 app.config['CORS_SUPPORTS_CREDENTIALS'] = True
 
 db = MongoEngine()
@@ -31,26 +34,37 @@ CORS(app)
 
 jwt = JWTManager(app)
 
+@app.after_request
+def refresh_expiring_jwts(response):
+    try:
+        claims = get_jwt()
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=10))
+        if target_timestamp > claims['exp']:
+            username = claims.get('username')
+            user_obj = BioGenomeUser.objects(name=username).first()
+            if user_obj:
+                access_token = create_access_token(identity=get_jwt_identity(),additional_claims={"role": user_obj.role.value, "username":user_obj.name})
+                set_access_cookies(response, access_token)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original response
+        return response
+
 username = os.getenv('DB_USER')
 password = os.getenv('DB_PASS')
 
-##create root user if does not exist
 try:
     FIRST_START = SingleInstance()
+
+    ##create root user if does not exist
     user = BioGenomeUser.objects(name = username).first()
+    if not user:
+        BioGenomeUser(name = username, password = password, role= Roles.DATA_ADMIN).save()
+
     cronjobs = CronJob.objects().count()
     if cronjobs:
         CronJob.drop_collection()
-    if not user:
-        BioGenomeUser(name = username, password = password, role= Roles.DATA_ADMIN).save()
-    # BioSample.drop_collection()
-    # TaxonNode.drop_collection()
-    # Organism.drop_collection()
-    # LocalSample.drop_collection()
-    # Experiment.drop_collection()
-    # Assembly.drop_collection()
-    # Chromosome.drop_collection()
-    # SampleCoordinates.drop_collection()
-    # GenomeAnnotation.drop_collection()
+
 except:
     pass
